ecb2f054ea950758d10e2727e8bd6986b7318e4b
Danish <Danish@DESKTOP-6AHN5Q4>
2019-06-22 13:08:23 +0530

    22 June 2019 Automation screenshot

--- src/Rough/BaseClassRough.java
+++ src/Rough/BaseClassRough.java
@@ -1,12 +1,20 @@
 package Rough;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.server.handler.GetScreenOrientation;
 
 public class BaseClassRough {
 
@@ -18,13 +26,12 @@ public class BaseClassRough {
 	public void Setup() throws IOException, InterruptedException {
 
 		// loading config properties
-		fis = new FileInputStream(
-				"E:\\eclipse-jee-neon-3-win32-x86_64\\New folder\\Automation\\src\\Properties\\config.properties");
+		
+		fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\Properties\\config.properties");
 		config.load(fis);
 
 		// loading OR properties
-		fis = new FileInputStream(
-				"E:\\eclipse-jee-neon-3-win32-x86_64\\New folder\\Automation\\src\\Properties\\OR.properties");
+		fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\Properties\\OR.properties");
 		OR.load(fis);
 		// instentiating browser
 
@@ -36,9 +43,37 @@ public class BaseClassRough {
 		driver.manage().window().maximize();
 		driver.get(config.getProperty("baseurl"));
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
+
 	}
 
+	public void login() throws IOException, InterruptedException {
+
+		BaseClassRough base = new BaseClassRough();
+
+		base.Setup();
+		driver.findElement(By.id(OR.getProperty("username_id"))).sendKeys(config.getProperty("username"));
+		driver.findElement(By.id(OR.getProperty("password_id"))).sendKeys(config.getProperty("password"));
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id(OR.getProperty("login_button_id"))).click();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	public void captureSnapShot() throws IOException {
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+		File ssFile = ((TakesScreenshot) (driver)).getScreenshotAs(org.openqa.selenium.OutputType.FILE);
+		FileUtils.copyFile(ssFile, new File(
+				"E:\\eclipse-jee-neon-3-win32-x86_64\\New folder\\Automation\\src\\excel\\" + sdf.format(d) + ".jpg"));
+	}
+
+	
+	public void dddsssss() throws IOException{
+		Date dd= new Date();
+		SimpleDateFormat sdff = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+		File ssfile =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(ssfile, new File("E:\\eclipse-jee-neon-3-win32-x86_64\\New folder\\Automation\\src\\excel\\" + sdff.format(dd) + ".jpg"));
+		
+	}
 	public void Close() {
 
 		driver.quit();
--- src/Rough/TestCaseRough.java
+++ src/Rough/TestCaseRough.java
@@ -1,36 +1,81 @@
 package Rough;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
+import java.util.List;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestCaseRough extends BaseClassRough {
 
 	BaseClassRough base = new BaseClassRough();
+	int count =-1;
 
-	@Test
-	public void login() throws IOException, InterruptedException {
-		base.Setup();
+	@BeforeTest
+	public void openapp() throws IOException, InterruptedException {
+		base.login();
+		Assert.assertEquals("https://opensource-demo.orangehrmlive.com/index.php/dashboard", driver.getCurrentUrl());
 
-		driver.findElement(By.id(OR.getProperty("username_id"))).sendKeys(config.getProperty("username"));
-		driver.findElement(By.id(OR.getProperty("password_id"))).sendKeys(config.getProperty("password"));
-		driver.findElement(By.xpath(OR.getProperty("login_button_xpath"))).click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		String URL = driver.getCurrentUrl();
-		
-		Assert.assertEquals(URL,"https://www.cryptobulls.exchange/authentication");
-		 
 		System.out.println("Login Successful!");
+
+	}
+
+	@Test(priority = 1)
+	public void operation() throws IOException, InterruptedException {
+
+		List<WebElement> list = driver.findElements(By.tagName("a"));
+		System.out.println("Number of links: " + list.size());
+		for (int i = 0; i < list.size(); i++) 
+		{
+			if (list.get(i).isDisplayed()) 
+			  {
+				count++;
+
+				System.out.println(list.get(i).getText());
+				String url = list.get(i).getText();
+				
+				FileInputStream inputStream = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\excel\\visblelinkURL1.xlsx");
+				XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+				
+				XSSFSheet s = wb.getSheet("Sheet1");
+				XSSFRow row = s.createRow(count);
+				row.createCell(0).setCellValue(url);
+				FileOutputStream outputStream = new FileOutputStream(
+						System.getProperty("user.dir") + "\\src\\excel\\visblelinkURL1.xlsx");
+				wb.write(outputStream);
+
+				wb.close();
+
+			}
+
+		}
+
+		int total = list.size();
+		int hidden = (total - count);
+		System.out.println("No of hidden link=" + hidden);
+		System.out.println("No of total link=" + total);
+		System.out.println("No of visible link=" + count);
+        
+		
+		
+	   
+	
+	}
+
+	@AfterTest
+	public void closeapp() {
 
 		base.Close();
 
 	}
 
-	
-		
-	}
-
-
+}
