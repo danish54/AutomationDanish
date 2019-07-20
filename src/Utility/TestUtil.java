427c161b256d5cf77dcd514459a3d1ef4b35800f
Danish <Danish@DESKTOP-6AHN5Q4>
2019-07-20 13:14:27 +0530

    20

---
+++ src/Utility/TestUtil.java
@@ -0,0 +1,24 @@
+package Utility;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.TakesScreenshot;
+
+import Rough.BaseClassRough;
+
+public class TestUtil extends BaseClassRough {
+	
+	public static void capturescreenshot() throws IOException{
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+		File ssFile = ((TakesScreenshot) (driver)).getScreenshotAs(org.openqa.selenium.OutputType.FILE);
+		FileUtils.copyFile(ssFile, new File(
+				"E:\\eclipse-jee-neon-3-win32-x86_64\\New folder\\Automation\\src\\excel\\" + sdf.format(d) + ".jpg"));
+		
+	}
+
+}
