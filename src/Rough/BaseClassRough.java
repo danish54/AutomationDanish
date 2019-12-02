56c34b15b2a275dab673d435842a0bf09a15ccf2
Danish <Danish@DESKTOP-6AHN5Q4>
2019-12-02 15:12:40 +0530

    minor change

--- src/Rough/BaseClassRough.java
+++ src/Rough/BaseClassRough.java
@@ -41,7 +41,7 @@ public class BaseClassRough {
 	public void Setup() throws IOException, InterruptedException {
 
 		// loading config properties
-		
+
 		fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\Properties\\config.properties");
 		config.load(fis);
 
@@ -59,7 +59,6 @@ public class BaseClassRough {
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
-		
 
 	}
 
@@ -86,142 +85,134 @@ public class BaseClassRough {
 	}
 
 	public void clickbyxpath(String xpath) {
-		
-		driver.findElement(By.xpath(xpath)).click();;
-		//BaseClassRough xp = new BaseClassRough();
-		//xp.clickbyxpath("click_xpath");
+
+		driver.findElement(By.xpath(xpath)).click();
+		;
+		// BaseClassRough xp = new BaseClassRough();
+		// xp.clickbyxpath("click_xpath");
 	}
-	
-public void sendbyxpath(String xpath, String value) {
-		
+
+	public void sendbyxpath(String xpath, String value) {
+
 		driver.findElement(By.xpath(xpath)).sendKeys(value);
-		//BaseClassRough xp = new BaseClassRough();
-		//xp.sendbyxpath("send_xpath", "value");
+		// BaseClassRough xp = new BaseClassRough();
+		// xp.sendbyxpath("send_xpath", "value");
 	}
-	
 
-public  boolean isElementPresent(WebDriver driver,By by)  
- {  
-    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);  
-    try  
-    {  
-       driver.findElement(by);  
-       return true;  
-    }  
-    
-    catch(Exception e)  
-    
-    {  
-    	
-       return false;  
-    }  
-    
-    finally  
-    {  
-    	
-       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);  
-     }  
- }  
+	public boolean isElementPresent(WebDriver driver, By by) {
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		try {
+			driver.findElement(by);
+			return true;
+		}
 
+		catch (Exception e)
 
-    @Test
+		{
+
+			return false;
+		}
+
+		finally {
+
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		}
+	}
+
+	@Test
 	public void emailSend() {
-    	
-    	
-    		 
-    		// Create object of Property file
-    		Properties props = new Properties();
-     
-    		// this will set host of server- you can change based on your requirement 
-    		props.put("mail.smtp.host", "smtp.gmail.com");
-     
-    		// set the port of socket factory 
-    		props.put("mail.smtp.socketFactory.port", "465");
-     
-    		// set socket factory
-    		props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
-     
-    		// set the authentication to true
-    		props.put("mail.smtp.auth", "true");
-     
-    		// set the port of SMTP server
-    		props.put("mail.smtp.port", "465");
-     
-    		// This will handle the complete authentication
-    		Session session = Session.getDefaultInstance(props,
-     
-    				new javax.mail.Authenticator() {
-     
-    					protected PasswordAuthentication getPasswordAuthentication() {
-     
-    					return new PasswordAuthentication("add your email", "add your password");
-     
-    					}
-     
-    				});
-     
-    		try {
-     
-    			// Create object of MimeMessage class
-    			Message message = new MimeMessage(session);
-     
-    			// Set the from address
-    			message.setFrom(new InternetAddress("mukeshotwani.50@gmail.com"));
-     
-    			// Set the recipient address
-    			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("mukesh.otwani50@gmail.com"));
-                
-                            // Add the subject link
-    			message.setSubject("Testing Subject");
-     
-    			// Create object to add multiMedia type content
-    			BodyPart messageBodyPart1 = new MimeBodyPart();
-     
-    			// Set the body of email
-    			messageBodyPart1.setText("This is message body");
-     
-    			// Create another object to add another content
-    			MimeBodyPart messageBodyPart2 = new MimeBodyPart();
-     
-    			// Mention the file which you want to send
-    			String filename = "G:\\a.xlsx";
-     
-    			// Create data source and pass the filename
-    			DataSource source = new FileDataSource(filename);
-     
-    			// set the handler
-    			messageBodyPart2.setDataHandler(new DataHandler(source));
-     
-    			// set the file
-    			messageBodyPart2.setFileName(filename);
-     
-    			// Create object of MimeMultipart class
-    			Multipart multipart = new MimeMultipart();
-     
-    			// add body part 1
-    			multipart.addBodyPart(messageBodyPart2);
-     
-    			// add body part 2
-    			multipart.addBodyPart(messageBodyPart1);
-     
-    			// set the content
-    			message.setContent(multipart);
-     
-    			// finally send the email
-    			Transport.send(message);
-     
-    			System.out.println("=====Email Sent=====");
-     
-    		} catch (MessagingException e) {
-     
-    			throw new RuntimeException(e);
-     
-    		}
-     
-    	}
-    	
 
-	
+		// Create object of Property file
+		Properties props = new Properties();
+
+		// this will set host of server- you can change based on your requirement
+		props.put("mail.smtp.host", "smtp.gmail.com");
+
+		// set the port of socket factory
+		props.put("mail.smtp.socketFactory.port", "465");
+
+		// set socket factory
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+		// set the authentication to true
+		props.put("mail.smtp.auth", "true");
+
+		// set the port of SMTP server
+		props.put("mail.smtp.port", "465");
+
+		// This will handle the complete authentication
+		Session session = Session.getDefaultInstance(props,
+
+				new javax.mail.Authenticator() {
+
+					protected PasswordAuthentication getPasswordAuthentication() {
+
+						return new PasswordAuthentication("add your email", "add your password");
+
+					}
+
+				});
+
+		try {
+
+			// Create object of MimeMessage class
+			Message message = new MimeMessage(session);
+
+			// Set the from address
+			message.setFrom(new InternetAddress("mukeshotwani.50@gmail.com"));
+
+			// Set the recipient address
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("mukesh.otwani50@gmail.com"));
+
+			// Add the subject link
+			message.setSubject("Testing Subject");
+
+			// Create object to add multiMedia type content
+			BodyPart messageBodyPart1 = new MimeBodyPart();
+
+			// Set the body of email
+			messageBodyPart1.setText("This is message body");
+
+			// Create another object to add another content
+			MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+
+			// Mention the file which you want to send
+			String filename = "G:\\a.xlsx";
+
+			// Create data source and pass the filename
+			DataSource source = new FileDataSource(filename);
+
+			// set the handler
+			messageBodyPart2.setDataHandler(new DataHandler(source));
+
+			// set the file
+			messageBodyPart2.setFileName(filename);
+
+			// Create object of MimeMultipart class
+			Multipart multipart = new MimeMultipart();
+
+			// add body part 1
+			multipart.addBodyPart(messageBodyPart2);
+
+			// add body part 2
+			multipart.addBodyPart(messageBodyPart1);
+
+			// set the content
+			message.setContent(multipart);
+
+			// finally send the email
+			Transport.send(message);
+
+			System.out.println("=====Email Sent=====");
+
+		} catch (MessagingException e) {
+
+			throw new RuntimeException(e);
+
+		}
+
+	}
+
 	public void Close() {
 
 		driver.quit();
